5ac64f6acb3a41ece03fd974c33883433ce4d380
OscarRAmirez <oerm16@gmail.com>
2020-05-28 07:32:43 -0500

    ProgramacionFuncional

---
+++ src/programacionfuncional/ProgramacionFuncional.java
@@ -0,0 +1,13 @@
+
+package programacionfuncional;
+
+public class ProgramacionFuncional {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
